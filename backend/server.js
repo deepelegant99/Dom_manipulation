6e0136bd38602ade47104f5d44562371aa16e634
Phillip Wong <deepelegant99@gmail.com>
2021-11-09 00:14:30 -0800

    I added a database so that the form set data to the server that in turn is put in a database.

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const app = express();
+const Datastore = require('nedb')
 const port = 3000;
+
+const database = new Datastore('database.db')
+database.loadDatabase();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+//This is for getting data from the form. 
 app.use(express.json());
- 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/", function (req, res) {
-  console.log(req.body);
+  database.insert(req.body);
   res.send("Got a POST request");
 });
 
