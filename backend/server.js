a6c08baedab34474269fe06fac73875353ebe08d
Phillip Wong <deepelegant99@gmail.com>
2021-11-09 00:30:23 -0800

    Setup so that the server/backend delivers the frontend files such as index.html

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,16 @@ const app = express();
 const Datastore = require('nedb')
 const port = 3000;
 
+//Setting up the database, and loading the database
 const database = new Datastore('database.db')
 database.loadDatabase();
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
 //This is for getting data from the form. 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//This is setup so that we server is serving the frontend. The frontend also contains the index.html file.  
+app.use(express.static("../frontend"));
 
 app.post("/", function (req, res) {
   database.insert(req.body);
