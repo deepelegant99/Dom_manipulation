fd94cadf3774fa93f88b8c740bc7c291866295e1
Phillip Wong <deepelegant99@gmail.com>
2021-11-09 00:35:47 -0800

    After the form data is send to the backend, the page redirct back to the form

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 //This is setup so that we server is serving the frontend. The frontend also contains the index.html file.  
 app.use(express.static("../frontend"));
 
+// This is so that the form can send the server something, and then modifying the database. 
 app.post("/", function (req, res) {
   database.insert(req.body);
-  res.send("Got a POST request");
+  res.redirect('/');
 });
 
 app.listen(port, () => {
